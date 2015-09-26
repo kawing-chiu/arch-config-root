b2c642add6bffb572082fa39cd9fde08d1ed2c9c
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-09-26 11:01:27 +0800

    Improve install_syslinux_uefi script.

--- opt/scripts/install_syslinux_uefi.py
+++ opt/scripts/install_syslinux_uefi.py
@@ -18,10 +18,7 @@ def _get_device(path):
     assert df_out[0] == 'Filesystem', "There is something wrong with df's output."
     full_dev_path = df_out[1]
 
-    print(full_dev_path)
-    match = re.search(r'(/dev/[a-z]+)([1-9])', full_dev_path)
-    return match.groups()
-
+    return full_dev_path
 
 def copy_syslinux_files(path):
     if not os.path.exists(path):
@@ -40,7 +37,10 @@ def set_efi_boot_entry(path):
     print("Current boot entries:")
     subprocess.call(['efibootmgr'])
 
-    device, partition = _get_device(path)
+    full_dev_path = _get_device(path)
+    match = re.search(r'(/dev/[a-z]+)([1-9])', full_dev_path)
+    device, partition = match.groups()
+
     print("Adding new EFI boot entry...")
     subprocess.call(['efibootmgr', '-c', '-d', device, '-p', partition,
         '-l', os.path.join('/', TARGET_PATH, 'syslinux/syslinux.efi'),
@@ -58,7 +58,7 @@ def copy_boot_files(arch_path):
         shutil.copy(os.path.join('/boot/', file_), arch_path)
 
 def create_syslinux_cfg(syslinux_path):
-    cfg_file = os.path.join(syslinux_path, 'syslinux.cfg')
+    print("Generating syslinux config...")
     cfg_template = """
     DEFAULT arch
     SAY Syslinux EFI now loading.....
@@ -67,15 +67,33 @@ def create_syslinux_cfg(syslinux_path):
     PROMPT 1
 
     LABEL arch
-        KERNRL ../arch/vmlinuz-linux
-        APPEND ro cryptdevice=UUID= root= intel_iommu=on
+        KERNEL ../arch/vmlinuz-linux
+        APPEND ro cryptdevice=UUID={crypt_uuid}:{crypt_name} root={root_path} intel_iommu=on
         INITRD ../arch/initramfs-linux.img
     """
+
+    crypt_path = input("Please input the path of the encrypted partition, "
+            "i.e. /dev/sda3: ").strip()
+    crypt_uuid = subprocess.check_output(['blkid', '-s', 'UUID', '-o', 'value', crypt_path])
+    crypt_uuid = crypt_uuid.rstrip().decode()
+    crypt_name = input("The name of the crypt device, i.e. luks_on_sdxc: ")
+    root_path = _get_device('/')
+
+    cfg_template = cfg_template.format(crypt_uuid=crypt_uuid, crypt_name=crypt_name, root_path=root_path)
+    lines = cfg_template.splitlines()
+    lines = map(lambda x: x[4:], lines)
+    cfg_template = '\n'.join(lines)
+
+    cfg_file = os.path.join(syslinux_path, 'syslinux.cfg')
+    with open(cfg_file, 'w') as f:
+        f.write(cfg_template)
 
 def _parse_args():
     from argparse import ArgumentParser
     parser = ArgumentParser(description="")
     parser.add_argument('mount_point', help="mount point of EFI partition, e.g. /boot/efi")
+    parser.add_argument('-u', '--update-kernel', action='store_true',
+            help="update kernel only without installing syslinux")
     args = parser.parse_args()
     if not os.path.ismount(args.mount_point):
         sys.exit("Error: '{}' is not a mount point".format(args.mount_point))
@@ -88,10 +106,15 @@ def run():
     syslinux_path = os.path.join(path, 'syslinux')
     arch_path = os.path.join(path, 'arch')
 
-    copy_syslinux_files(syslinux_path)
-    set_efi_boot_entry(path)
-    copy_boot_files(arch_path)
-    #create_syslinux_cfg(syslinux_path)
+    if args.update_kernel:
+        copy_boot_files(arch_path)
+    else:
+        copy_syslinux_files(syslinux_path)
+        set_efi_boot_entry(path)
+        copy_boot_files(arch_path)
+        create_syslinux_cfg(syslinux_path)
+
+    print("Done.")
 
 
 if __name__ == "__main__":
