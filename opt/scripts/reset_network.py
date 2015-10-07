8c292b4e13099eed540df2736353dd8c66ee38d8
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-10-08 07:52:27 +0800

    Add reset_network.py script.

---
+++ opt/scripts/reset_network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Restart network services.
+"""
+from subprocess import call
+
+
+def _parse_args():
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="")
+    args = parser.parse_args()
+    return args
+
+def run():
+    args = _parse_args()
+
+    print("Reconnecting wifi...")
+    call(['systemctl', 'restart', 'netctl-auto@*'])
+    print("Reloading unbound...")
+    call(['unbound-control', 'reload'])
+    #print("Restarting goagent...")
+    #call(['systemctl', 'restart', 'goagent'])
+
+if __name__ == '__main__':
+    run()
