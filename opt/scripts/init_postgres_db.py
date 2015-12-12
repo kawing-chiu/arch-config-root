0a83869c87c8780d168469e134aed2fa44cadb85
Zhao Jiarong <kawing.chiu.sysu@gmail.com>
2015-12-12 13:57:20 +0800

    Add init_postgres_db script.

---
+++ opt/scripts/init_postgres_db.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from _utils import *
+
+
+def run():
+    path = input("Path of the new database: ")
+    cmd = ['initdb', '--locale', 'en_US.UTF-8', '-E', 'UTF8', '-D', path]
+
+    is_root = os.geteuid() == 0
+    if is_root:
+        user = input("Create database as user: ")
+        if user == 'root':
+            sys.exit("Error: please don't create database as root.")
+        call_as_user(user, cmd)
+    else:
+        call(cmd)
+
+if __name__ == "__main__":
+    run()
